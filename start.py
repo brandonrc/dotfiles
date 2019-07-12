fd10158e46b729f2644d9b7bd458c93fe81b0ed9
root <root@portainer.lars>
2019-07-12 23:01:29 +0000

    update

---
+++ start.py
@@ -0,0 +1,20 @@
+# /bin/python3
+
+import os
+import random
+import string
+
+def randomStringDigits(stringLength=10):
+    """Generate a random string of letters and digits """
+    lettersAndDigits = string.ascii_letters + string.digits
+    return ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
+
+#Variables
+port = random.randint(30000,50000)
+name = randomStringDigits(10)
+
+startDocker = 'docker run -d --name ' + str(name) + ' -p ' + str(port) + ':3389 kxes/desktop'
+
+print(startDocker)
+
+os.system(startDocker)
